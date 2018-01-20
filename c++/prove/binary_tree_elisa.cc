07eb17caeb6fb4fa17ed41d1699c0750eba38690
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-01-20 09:39:42 +0100

    Trying..

--- c++/prove/binary_tree_elisa.cc
+++ c++/prove/binary_tree_elisa.cc
@@ -1,24 +1,48 @@
-#include <iostream>
-#include <memory> // needed for smart pointers
+#include <iostream> // std::cout
+#include <memory> // std::unique_ptr
+#include <utility> // std::pair, std::make_pair
+//#include <string> // std::string
+#include <iterator>
 
 // **************************************************************************
 // FUNCTION DECLARATION AND DEFINITION
 // **************************************************************************
+/*
+(1) insert, used to insert a new pair key-value. (Alberto ne ha fatte 2)
+(2) print, used to print key: value of each node. Note that the output should be in order with respect to the keys.
+(3) clear(), clear the content of the tree. (DA zero)
+(4) begin(), return an iterator to the first node (which likely will not be the root node)
+(5) end(), return a proper iterator
+(6) cbegin(), return a const_iterator to the first node (da zero)
+(7) cend(), return a proper const_iterator ( da zero)
+(8) balance(), balance the tree. (da zero, sorting con funzione della std::lib)
+(9) find, find a given key and return an iterator to that node. If the key is not found returns end(); (simile al print)
+ */
+// ------------------------ CLASS TREE STARTS ----------------------------
 template<typename kt, typename vt>
 class Tree
 {
 private:
-  struct Node // (1) 
+  struct Node // (1) Structure Node
   {
     std::unique_ptr<Node> left,right;
     Node *up;
     kt key;
     vt value;
-    Node(const kt& k, const vt& v, Node* u):left{nullpt}, right{nullptr},
-					    key{k}, value{v}, up{u} {}
-  }
+    std::pair <kt,vt> key_value;
+    
+    // Constructor for the node
+    Node(const kt& k, const vt& v, Node* u): left{nullptr}, right{nullptr},
+					     key{k}, value{v}, up{u} {}
+  };
   // ------------------------------------------------------------------------
-    Iterator insert(const kt& k, const vt& v) // (2) 
+public:
+  class Iterator; // (4) Class Iterator
+  Iterator begin() {return Iterator {root.get()};}
+  Iterator end() {return Iterator {nullptr};}
+  
+private:
+  Iterator insert(const kt& k, const vt& v) // (2) Function Insert 
   {
     if(k<key)
       {
@@ -32,30 +56,41 @@ private:
       }
     else if(k>key)
       {
-	if(righy==nullptr)
+	if(right==nullptr)
 	  right.reset(new Node{k,v,up});
 	else
 	  right->insert(k,v);
       }
     else
-      return iterator{this};
+      return Iterator{this};
   }
-  // **************************************************************************
+  // -------------------------------------------------------------------- 
+  // --------------------------------------------------------------------
 public:
-  std::unique_ptr<Node> root=nullptr; // (3)
+  Tree(): std::unique_ptr<Node>root{nullptr} {}; // (3) Constructor of the Tree
   // ------------------------------------------------------------------------
-  Iterator insert(const kt&k, const vt&v) // (4) 
+ 
+  
+  /* We should also add:
+     class ConstIterator;
+     ConstIterator  begin() {return ConstIterator {root.get()};}
+     ConstIterator  end() {return ConstIterator {nulptr};}
+  */
+  Iterator insert(const kt&k, const vt&v) // (5) Function Insert 
   { 
     if(root==nullptr)
       {
 	root.reset(new Node{k,v,nullptr});
 	return Iterator{root.get()};
 	else
+	  {
 	  return root->insert(k,v);
+	  }
       }
   }
   // ------------------------------------------------------------------------
-  void print() // (5)
+  // The function PRINT should be CONST anyway ...
+  void print()/*const*/ // (6)
   { 
     if(root==nullptr)
       std::cout << "Empy tree\n";
@@ -65,52 +100,86 @@ public:
     auto last = end(); //{return iterator{root->up}
     // auto it {this->begin()};
     // auto last {this->end()};
-    for(; it!=last; ++it)
+    for(; it!=last; ++it) // Doesn't need the first argument since "it" is already intialized
       {
+	/* If we are using pair, we will write:*/
 	*it.first;
 	*it.second;
-	it->first;
-	// std::cout <<*it <<"\n";
+	it->first;      
+	// If we do not use std::pair
+	// std::cout << *it <<"\n";
       }
   }
+};
+// ------------------------ CLASS TREE ENDS ----------------------------
+
+// ************************************************************************
+// ************************************************************************
+template<typename kt, typename vt>
+class Tree<kt,vt>::Iterator
+{
+private:
+  using Node = Tree<kt,vt>::Node; // alias
+  Node *current;
   // -------------------------------------------------------------------- 
   // --------------------------------------------------------------------
-  template<typename kt, typename vt>
-  class Tree<kt,vt>::Iterator // (5) 
+public: 
+  Iterator(Node* n), current{n}{} // (a) Constructor of the Iterator
+    
+  std::pair<kt,vt> operator->() {return (*this);} // (b) 
+  std::pair<kt,vt> operator*() {return{current->key, current->value};} // (c)
+    
+  Iterator& operator++() // (d)
   {
-  private:
-    using Node = Tree<kt,vt>::Node; // ????
-    Node *current;
-    // ************************************************************************
-  public: 
-    Iterator(Node* n), current{n}{} // (a) 
-    
-    std::pair<kt,vt> operator->() {return (*this);} // (b) 
-    std::pair<kt,vt> operator*() {return{current->key, current->value};} // (c)
-    
-    Iterator& operator++() // (d)
-    {
-      current = current->next.get(); //update current
-      return *this; // return this
-    }
-
-    Iterator operator++(int) // (e) 
-    {
-      Iterator it{current}
-      ++(*this); // update current
-      return it; // return other
-    }
-    
-  }
-  // -------------------------------------------------------------------- 
-  // --------------------------------------------------------------------
-    void Node::print_recursive()
-  {
-    if(left!=nullptr)
-      left->print_recursive();
-    std::cout<< key << "" << value << "\n";
-    
-    if(right!=nullptr)
-      right->print_recursive();
+    current = current->up.get(); //update current
+    return *this; // return this
   }
 
+  Iterator operator++(int) // (e) 
+  {
+    Iterator it{current}
+    ++(*this); // update current
+    return it; // return other
+  }
+    
+};
+// -------------------------------------------------------------------- 
+// --------------------------------------------------------------------
+void Node::print_recursive()
+{
+  if(left!=nullptr)
+    left->print_recursive();
+  std::cout<< key << "" << value << "\n";
+    
+  if(right!=nullptr)
+    right->print_recursive();
+}
+
+/*
+std::ostream& operator<<(std::ostream& os, const Month& m) //(7e)
+{
+  return os;
+}
+*/
+// ************************************************************************
+// MAIN
+// ************************************************************************
+int main()
+{
+  Tree<int>tree{};
+  tree.insert(1);
+  tree.insert(2);
+  tree.insert(3);
+
+  //  list.print();
+  /*
+    btree b;
+    int tmp;
+  
+    std::cout << "Enter the number to be inserted\n";
+    std::cin>>tmp;
+    b.insert(tmp);
+  */
+
+  return 0;
+}
