5ffbc22d2497fe7e8f3d4704c7ea34ceb5fd4ed6
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-01-22 00:30:18 +0100

    Merging my file with Auro's one

---
+++ c++/prove/5_binary_tree_elisa.cc
@@ -0,0 +1,193 @@
+#include <iostream> // std::cout
+#include <memory> // std::unique_ptr
+#include <utility> // std::pair, std::make_pair
+#include <iterator>
+
+using std::cout;
+using std::endl;
+// **************************************************************************
+// FUNCTION DECLARATION AND DEFINITION
+// **************************************************************************
+/*
+  (1) insert, used to insert a new pair key-value. (Alberto ne ha fatte 2)
+  (2) print, used to print key: value of each node. Note that the output 
+  should be in order with respect to the keys.
+  (3) clear(), clear the content of the tree. (DA zero)
+  (4) begin(), return an iterator to the first node (which likely will not 
+  be the root node)
+  (5) end(), return a proper iterator
+  (6) cbegin(), return a const_iterator to the first node (da zero)
+  (7) cend(), return a proper const_iterator ( da zero)
+  (8) balance(), balance the tree. (da zero, sorting con funzione della std::lib)
+  (9) find, find a given key and return an iterator to that node. If the key 
+  is not found returns end(); (simile al print)
+*/
+template <typename kt, typename vt> class Node;
+template <typename kt, typename vt> class Tree; 
+template <typename kt, typename vt> class Iterator; 
+
+// ************************ CLASS NODE STARTS ************************
+template <typename kt, typename vt>
+class Node 
+{
+public:
+  kt key;
+  vt value;
+  Node *left;
+  Node *right;
+  Node *up;
+  Node(const kt& k, const vt& v, Node* l, Node*r, Node* u):
+    key{k}, value{v}, left{nullptr}, right{nullptr}, up{nullptr} {}
+};
+// ************************ CLASS NODE ENDS  ************************
+
+// ------------------------ CLASS TREE STARTS ----------------------------
+template<typename kt, typename vt>
+class Tree
+{
+private:
+  Node<kt,vt>* root;
+  unsigned int _size;
+  Node<kt,vt>* insert_helper(kt key, vt val, Node<kt,vt>* t);
+  void clear_helper(Node<kt,vt>* n);
+
+  
+public:
+  Tree(); 
+  class Iterator; 
+  Iterator begin() {return Iterator {root.get()};}
+  Iterator end() {return Iterator {nullptr};}
+  class ConstIterator; 
+  ConstIterator cbegin() const {return ConstIterator {root.get()};}
+  ConstIterator cend() const {return ConstIterator {nullptr};}
+  void insert_noiter(kt key, vt val) {root = insert_helper(key, val, root);}
+  void clear() {clear_helper(root);}
+
+};
+// ------------------------ CLASS TREE ENDS ----------------------------
+template<typename kt, typename vt>
+Tree<kt,vt>::Tree()
+{  
+  _size=0; 
+  root=nullptr;
+}
+// ----------------------------------------------------------------------
+template<typename kt, typename vt>
+Node<kt,vt>* Tree<kt,vt>::insert_helper(kt key, vt val, Node<kt,vt>* t)
+{
+  if(t == nullptr) 
+    {
+      cout << "creazione nodo: " << key << endl;
+      //   t = new Node<kt,vt> (key,val,nullptr); //  --> by AURO, but error
+      t = new Node<kt,vt> (key,val,nullptr,nullptr, nullptr);
+      _size = _size+1;        
+    }
+  else if(key < t->key){        
+    t->left = insert_helper(key, val, t->left);
+    t->left->up = t;
+  }
+  else if(key > t->key){ 
+    t->right = insert_helper(key, val, t->right);
+    t->right->up = t;
+  }
+  return t;
+}
+// ----------------------------------------------------------------------
+template <typename kt, typename vt>
+void Tree<kt,vt>::clear_helper(Node<kt,vt>* n){
+  if( n == nullptr) return;
+    
+  clear_helper(n->left);// if(n->left != nullptr) clear_helper(n->left);
+  clear_helper(n->right);// if(n->left != nullptr) clear_helper(n->left);
+    
+  delete n;
+  n = nullptr;
+}
+// ----------------------------------------------------------------------
+
+// ************************ ITERATOR STARTS ************************
+template<typename kt, typename vt>
+class Tree<kt,vt>::Iterator
+{
+private:
+  Node<kt,vt> *current;
+  
+public:  
+  Iterator(Node<kt,vt>* n): current{n}{} 
+  std::pair<kt,vt> operator->() {return (*this);} 
+  std::pair<kt,vt> operator*() const {return{current->key, current->value};}
+
+  // ++it
+  Iterator& operator++() 
+  {
+    current = current->up.get(); 
+    return *this; 
+  }
+  // it++
+  Iterator operator++(int)
+  {
+    Iterator it{current};
+    ++(*this); 
+    return it; 
+  }
+
+  bool operator==(const Iterator& other)
+  {
+    return this->current==other.current;
+  }
+  
+  bool operator!=(const Iterator& other) {return !(*this == other);}
+};
+// ************************ ITERATOR ENDS ************************
+// ++++++++++++++++++ CONST ITERATOR STARTS ++++++++++++++++++
+template<typename kt, typename vt> 
+class Tree<kt,vt>::ConstIterator:public Tree<kt,vt>::Iterator
+{
+  using parent = Tree<kt,vt>::Iterator;
+public:
+  using parent::Iterator;
+  const std::pair<kt,vt> operator*() const {return parent::operator*();} 
+};
+// ++++++++++++++++++ CONST ITERATOR ENDS ++++++++++++++++++
+
+
+// ************************************************************************
+// MAIN
+// ************************************************************************
+int main()
+{
+  Tree<int, int>tree{};
+
+  cout << "Insert nodes... " << endl;
+  tree.insert_noiter(1,1);
+  tree.insert_noiter(2,2);
+  tree.insert_noiter(3,3);
+
+  /*
+    Tree<int, int>::Iterator last = tree.end();
+    for(Tree<int,int>::Iterator it = tree.begin(); it !=last; ++it)
+    {
+    cout << "The tree has 3 elements" << endl;
+    }
+  */
+      
+  tree.clear();
+  return 0;
+}
+
+
+/*
+  ALTRE DOMANDE PER AURORA
+  (1) Ma se mettiamo la classe Nodo all'interno della classe Tree
+  templata su kt e vt
+  anche il Nodo erediterà il fatto di essere templato?
+
+  (2) Dobbiamo templare anche il Nodo?
+  Se provo a NON templarlo ad un certo punto mi fa:
+
+  5_binary_tree_elisa.cc:111:1: error: ‘Node’ does not name a type
+  Node* Tree<kt,vt>::insert_helper(kt key, vt val, Node* t)
+
+
+  (3) Warnings: non usa puntatori LEFT, RIGHT, UP
+*/
---
+++ c++/prove/binary_tree_aurora3.cc
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <memory> // needed for smart pointers
+
+using std::cout;
+using std::endl; //instead of loading all the std::functions, only these one,
+// for avoid possible conflicts
+
+// **************************************************************************
+// FUNCTION DECLARATION AND DEFINITION
+// **************************************************************************
+
+template <typename kt, typename vt> class Tree; //class declaration
+template <typename kt, typename vt> class Iterator; //class declaration needed
+// so that Tree compiles
+
+
+// *** BEGIN CLASS NODE ********
+
+template <typename kt, typename vt>
+class node //class node implementation (outside tree)
+{
+public:
+    
+    kt key;
+    vt val;
+    node* left;
+    node* right;
+    node* up = nullptr;
+    node();  //add default constructor senno nn compila ..
+  //          serve x classe standad vector
+    node (const kt& k = kt(), const vt& v = vt(), node* l = nullptr,
+	  node* r = nullptr): key(k),val(v),left(l), right(r) {}
+  //costructor con i valori di default
+//k and v are dummy variables that live only here
+};
+
+template <typename kt, typename vt>
+node<kt,vt>::node(){    //implementation of the default constructor
+    left=nullptr;
+    up=nullptr;
+    right=nullptr;
+    val = vt();
+    key = kt();  //default constructor explicitely use default
+    // constructors for the types kt, vt
+    
+}
+
+// *** END CLASS NODE ********
+
+
+// *** BEGIN CLASS TREE ********
+
+template<typename kt, typename vt>
+class Tree
+{
+    private:
+ 
+    Iterator<kt,vt> insert(const kt& k, const vt& v);
+  //since iterator class is defined outside Tree, here requires <kt,vt>  
+    //helper function private to implement recursion
+  // NON UTILIZZATA
+  
+    node<kt,vt>* insert_helper(kt key, vt val, node<kt,vt>* t);
+  //insert helper needs key value and root
+  //the first time (e.g. key 7, val 7) they are 7, 7, nullptr
+  //because I initialize with root=nullptr
+  
+    void clear_helper(node<kt,vt>* n);
+  //clear_helper needs a variable pointer to node
+  
+    node<kt,vt>* root;  //std::unique_ptr< node<kt,vt> > root=nullptr;
+    unsigned int _size; //private variable to keep tree size
+
+    
+    public:
+    
+    Tree();
+    void insert_noiter(kt key, vt val) {root = insert_helper(key, val, root);}
+  //insert_noiter recalls insert helper (wrapper of insert_helper)
+  //i called it insert_noiter because I do not use iteratorsyet.
+  
+       void clear() {clear_helper(root);}
+  //clear calls clear helper starting from the root
+  //if root does not exist it does return
+  //otherwise it checks left and then right until right == nulptr
+  //i need clear to delete the tree and have 0 leaks with valgrind
+  
+};
+
+template<typename kt, typename vt>
+Tree<kt,vt>::Tree(){  //default cosntructor for Tree
+    _size=0; // at the beginning the tree is empty, size = 0
+    root=nullptr;
+}
+
+
+template<typename kt, typename vt>
+node<kt,vt>*  Tree<kt,vt>::insert_helper(kt key, vt val, node<kt,vt>* t)
+//key,val,root ..insert helper ricorre dentro se stessa
+{
+    
+    if(t == nullptr) //if root == nullptr I create the first node 
+    {
+        cout << "creazione nodo: " << key << endl;
+        t = new node<kt,vt>(key,val,nullptr,nullptr);
+	//the first node has a key, a value and points to nullptr and
+	// nullptr left and right
+        _size = _size+1;
+	// when I create the first node the size of the tree increases
+	// and so on...
+  //every time i define a newnode a redefine the parent
+        
+    }
+    else if(key < t->key){
+      //if the key of the new node is smaller than put it left
+        
+        t->left = insert_helper(key, val, t->left);
+	//cout << "node current L "<< t->key << endl;
+        t->left->up = t;
+    }
+    else if(key > t->key){ //if the key of the new node is bigger
+      // than put it left
+        
+        t->right = insert_helper(key, val, t->right);
+	//cout << "node current R "<< t->key << endl;
+        t->right->up = t;
+    }
+    return t;
+}
+
+
+
+template <typename kt, typename vt>
+void Tree<kt,vt>::clear_helper(node<kt,vt>* n){
+    if( n == nullptr) return;
+    
+    clear_helper(n->left);// if(n->left != nullptr) clear_helper(n->left);
+    clear_helper(n->right);// if(n->left != nullptr) clear_helper(n->left);
+    
+    delete n;
+    n = nullptr;
+}
+
+// *** END CLASS TREE ********
+
+
+// *** BEGIN CLASS ITERATOR TREE ********
+
+template<typename kt, typename vt>
+class Iterator
+//  remove Tree<kt,vt>::Iterator because is defined outside Tree class
+{
+private:
+   
+    node<kt,vt> *current;
+    // ************************************************************************
+public:
+    Iterator(node<kt,vt>* n) : current(n){} //CHANGED IN THIS TO COMPILE
+    
+    std::pair<kt,vt> operator->() {return (*this);} // (b)
+    std::pair<kt,vt> operator*() {return{current->key, current->value};} // (c)
+    
+    Iterator& operator++() // (d)
+    {
+    }
+    
+    Iterator operator++(int) // (e)
+    {
+    }
+    
+};
+
+// *** END CLASS ITERATOR TREE ********
+
+int main(){
+    
+    Tree<int,int> t{};
+    
+    cout << "Insert nodes... " << endl;
+    t.insert_noiter(7,7); //insert_noiter(key,value)
+    t.insert_noiter(3,3);
+    t.insert_noiter(13,13);
+    t.insert_noiter(14,14);
+    t.insert_noiter(4,4);
+    
+    
+    t.clear();
+
+    
+}
+
+
